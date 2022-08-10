554db833f0748ce83b3074f1db98a3aaf48016b9
Anderson Taborga <anderson.taborga@gmail.com>
2022-08-10 19:26:08 -0300

    First Microservice - Catalog API with MongoDB

---
+++ src/Service/Catalog/Catalog.API/Data/ICatalogContext.cs
@@ -0,0 +1,12 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+using System.Collections.Generic;
+
+namespace Catalog.API.Data
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+        
+    }
+}
